01b4400c3cade55bf8a53a0d195179f5ad0ae553
Max Kramer <max@maxkdevelopment.co.uk>
2011-11-22 00:20:50 +0000

    Initial Commit

---
+++ JSON.h
@@ -0,0 +1,38 @@
+//
+//  NSObject+JSON.h
+//  Timbrr
+//
+//  Created by Max Kramer on 22/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (JSON)
+- (id) JSONValue;
+@end
+
+@implementation NSString (JSON)
+- (id) JSONValue {
+    NSError *error = nil;
+    id obj = [NSJSONSerialization JSONObjectWithData:[self dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:&error];
+    if (error)
+        NSLog(@"%@", [error description]);
+    return obj;
+} 
+@end
+
+@interface NSData (JSON)
+- (id) JSONValue;
+@end
+
+@implementation NSData (JSON)
+- (id) JSONValue {
+    NSError *error = nil;
+    id obj = [NSJSONSerialization JSONObjectWithData:self options:NSJSONReadingMutableContainers error:&error];
+    if (error)
+        NSLog(@"%@", [error description]);
+    return obj;
+}
+@end
+
